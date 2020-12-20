c663ea6de5ceb16e730211773d63c39a0fc44e8c
Chad Milburn <cmilburn@ballstate.bsu.edu>
2020-12-20 16:58:37 -0500

    commit on master

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,5 +1,5 @@
-# modules to work across os and with csv files
-import os
+# import modules
+import os 
 import csv
 
 # set path for file
@@ -8,30 +8,74 @@ csvpath = os.path.join("Resources", "budget_data.csv")
 # open csv file
 with open(csvpath) as csvfile:
 
-    #check for me that I have opened the correct file
+    
     # #csv reader with delimieter and variable to hold contents
     csvreader = csv.reader(csvfile, delimiter=",")
-
-    # #print(csvreader)
-
+    
+    #check that I am getting my file
+    #print(csvreader)
+    
+    #skips the header row
     csv_header = next(csvreader)
-    #print(f'CSV Header: {csv_header}')
-    # this is counting the months but now working correctly without the +1
-    total = 0
-    for row in csvreader:
-        total = float(row[1])
-        print(total) 
-         #print(row)
-        row_count = sum(1 for row in csvfile)+1
-        print(row_count)       
-
-    # for row in csvreader:
-    #     #print(row)
-    #     row_count = sum(1 for row in csvfile)+1
-    #     print(row_count) 
-
-     
     
+    #print(f'CSV Header:{csv_header}')
+    #check for me that I am pulling hte correct data 
+    #for row in csvreader:
+    #    print(row)
+
+    # create list of total months and total profit/loss
+    profit = []
+    months = []
+
+    for rows in csvreader:
+        #creates a new list for first column of data a row at a time
+        months.append(rows[0])
+        #creates a new list for second column of data a row at a time
+        profit.append(int(rows[1]))
+        #check for me that this worked
+        #print(months)
+        #print(profit)
+        total_months = len(months)
+        #print(total_months)
+        total_profitloss =sum(profit)
+        #print(total_profitloss)
 
 
-    
+        #create rev change list and calculate list by cycling each x and subtracitng the next months for change
+        rev_change =[]
+        for x in range(1, len(profit)):
+            rev_change.append(int(profit[x]) - int(profit[x-1]))
+            #print(rev_change)
+            great_increase = max(rev_change)
+            great_decline = min(rev_change)
+            ave_change = round((sum(rev_change)/len(rev_change)),2)
+            #checks for the above lines
+            #print(great_increase)
+            #print(great_decline)
+            #print(ave_change)
+            #print(months[rev_change.index(max(rev_change))+1])
+            #print(months[rev_change.index(min(rev_change))+1])
+            month_increase = (months[rev_change.index(max(rev_change))+1])
+            #print(month_increase)
+            month_decrease = (months[rev_change.index(min(rev_change))+1])
+            #print(month_decrease)
+
+ #creates and writes required info to txt file
+file = open("Financial_Analysis.txt","w")
+file.write("Financial Analysis\n")
+file.write("----------------------------------------\n")
+file.write(f'Total Months: {total_months}\n')
+file.write(f'Total: ${total_profitloss}\n')
+file.write(f'Average Change: $ {ave_change}\n')
+file.write(f'Greatest Increase in Profits: {month_increase} (${great_increase})\n')
+file.write(f'Greatest Decrease in Profits: {month_decrease} (${great_decline})\n')
+file.close()
+
+#print to terminal
+print("Financial Analysis")
+print("----------------------------------------")
+print(f'Total Months: {total_months}')
+print(f'Total: ${total_profitloss}')
+print(f'Average Change: $ {ave_change}')
+print(f'Greatest Increase in Profits: {month_increase} (${great_increase})')
+print(f'Greatest Decrease in Profits: {month_decrease} (${great_decline})')
