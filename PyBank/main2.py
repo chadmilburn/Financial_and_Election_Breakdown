007a02b3a2c4cdf57d7f4fae4a3004e9888d71aa
Chad Milburn <cmilburn@ballstate.bsu.edu>
2020-12-22 12:59:27 -0500

    updating new file

--- PyBank/main2.py
+++ PyBank/main2.py
@@ -19,12 +19,21 @@ with open(csvpath) as csvfile:
     #creates lists for months and profits from data by looping each row
     month = []
     profit = []
-    total_profit = 0 
+    
+    total_profit = 0
     for rows in csvreader:
         month.append(rows[0])
         profit.append(rows[1])
         total_profit += int(rows[1])
-
+    
+    profit_change =[]
+    for i in range(1, len(profit)):
+        profit_change.append(int(profit[i])- int(profit[i-1]))
+    greatest_increase = max(profit_change)
+    greatest_increase_month = profit_change.index(max(profit_change))+1
+    big_month = month[greatest_increase_month]
+    greatest_decrease = min(profit_change)
+    average_change = sum(profit_change)/len(profit_change)
     #check for correct list info
     #print(month)
     #print(profit)
@@ -34,5 +43,11 @@ with open(csvpath) as csvfile:
         
     
     
-    print(total_months)
-    print(total_profit)
+    print(f' Total Months:  {total_months}')
+    print(f' Total: ${total_profit}')
+    print(profit_change)
+    print(greatest_increase)
+    print(greatest_decrease)
+    print(average_change)
+    print(greatest_increase_month)
+    print(big_month)
